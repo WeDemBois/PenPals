c4586a05e738ff381a5d590697c697e9c517e4df
cjs07 <cjschaefer1999@gmail.com>
2017-10-16 22:08:28 -0400

    navigation part 2

--- app/src/main/java/com/github/wedemboys/penpals/NewChatActivity.java
+++ app/src/main/java/com/github/wedemboys/penpals/NewChatActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.android.volley.Request;
@@ -19,12 +20,23 @@ public class NewChatActivity extends Activity {
     EditText recipients;
     EditText chatText;
 
+    CheckBox random;
+    CheckBox interest;
+    CheckBox language;
+    CheckBox group;
+    EditText groupSize;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_chat);
         recipients = (EditText) findViewById(R.id.newChatRecipients);
         chatText = (EditText) findViewById(R.id.newChatMessageContent);
+        random = (CheckBox) findViewById(R.id.randomCheckbox);
+        interest = (CheckBox) findViewById(R.id.interestCheckbox);
+        language = (CheckBox) findViewById(R.id.languageCheckbox);
+        group = (CheckBox) findViewById(R.id.groupCheckbox);
+        groupSize = (EditText) findViewById(R.id.groupSize);
     }
 
     public void createChat(View view) {
@@ -63,7 +75,31 @@ public class NewChatActivity extends Activity {
     private void goToChats() {
         Intent intent = new Intent(this, ChatActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        
+
+        startActivity(intent);
+    }
+
+    public void onRandomChanged(View view) {
+        if (random.isChecked()) {
+            interest.setVisibility(View.VISIBLE);
+            language.setVisibility(View.VISIBLE);
+        } else {
+            interest.setVisibility(View.INVISIBLE);
+            language.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void onGroupChanged(View view) {
+        if (group.isChecked()) {
+            groupSize.setVisibility(View.VISIBLE);
+        } else {
+            groupSize.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void goBack(View view) {
+        Intent intent = new Intent(this, MainPageActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 }
